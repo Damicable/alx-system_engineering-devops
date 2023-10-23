8cd008677f3a79c305d545d2f5c834eb48be5861
Damicable <annobidiegwu@gmail.com>
2023-10-23 05:13:10 -0700

    0-gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,6 +4,7 @@
 import requests
 import sys
 
+
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
