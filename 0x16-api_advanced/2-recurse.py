c83daabb69ed0dd052bfd0295469324b62ea8835
Damicable <annobidiegwu@gmail.com>
2023-11-07 10:01:57 -0800

    Updated

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-
-"""This script defines a recursive function to query the Reddit API"""
+"""Module script to recursively returns list of hot posts of subreddit"""
 
 import requests
 import sys
 
-def recurse(subreddit, hot_list=None, after=None):
-    """Return all hot article titles"""
-    if hot_list is None:
-        hot_list = []
 
-    headers = {'User-Agent': 'MyRedditBot'}  # Add your user-agent here
-
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    params = {'limit': 100, 'after': after}
-
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code != 200:
-        return None  # Invalid subreddit or other error
-
-    data = response.json()
-    if 'data' in data and 'children' in data['data']:
-        for post in data['data']['children']:
-            hot_list.append(post['data']['title'])
-
-    after = data['data']['after']
-
-    if after:
-        return recurse(subreddit, hot_list, after)
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Return all hot article titles list
+    """
+    req_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "Switch value to prevent breaking"
+    }
+    params = {
+        "after": after
+    }
+    r = requests.get(
+            req_url, headers=headers, params=params, allow_redirects=False)
+    if r.status_code == 200:
+        bulk_resp = r.json()
+        children = bulk_resp.get('data').get('children')
+        new_after = bulk_resp.get('data').get('after')
+        titles = list(map(lambda x: x.get('data').get('title'), children))
+        hot_list.extend(titles)
+        if new_after is None:
+            return hot_list
+        return recurse(subreddit, hot_list, new_after)
     else:
-        return hot_list
+        return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     recurse(sys.argv[1])
