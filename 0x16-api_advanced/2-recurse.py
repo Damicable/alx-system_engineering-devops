3d05236d6c18d259ac5825c862f4bd7495ea9379
Damicable <annobidiegwu@gmail.com>
2023-11-07 13:04:22 -0800

    Recursions handled

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,12 +6,15 @@ This script defines a recursive function to query the Reddit API
 import requests
 import sys
 
-def recurse(subreddit, hot_list=[], after=None):
-    """
-    Return all hot article titles
-    """
 
-    headers = {'User-Agent': 'MyRedditBot/1.0'}  # Add your user-agent here
+def recurse(subreddit, hot_list=None, after=None):
+    """
+    Return a list of titles of all hot articles in the given subreddit
+    """
+    if hot_list is None:
+        hot_list = []
+
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
 
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     params = {'limit': 100, 'after': after}
@@ -19,7 +22,7 @@ def recurse(subreddit, hot_list=[], after=None):
     response = requests.get(url, headers=headers, params=params)
 
     if response.status_code != 200:
-        return None  # Invalid subreddit or other error
+        return None
 
     data = response.json()
     if 'data' in data and 'children' in data['data']:
@@ -35,5 +38,8 @@ def recurse(subreddit, hot_list=[], after=None):
 
 
 if __name__ == "__main__":
-    recurse(sys.argv[1])
+    subreddit = sys.argv[1]
+    result = recurse(subreddit)
 
+    for title in result:
+        print(title)
